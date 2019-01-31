d32b2fd8ef7bab3c5e25558ea3fc39d85214fd92
mate0905 <mate0905@gmail.com>
2019-01-31 16:55:08 +0100

    power

---
+++ CLionProjects/week 3 day 4 ( power ) /cmake-build-debug/CMakeFiles/week_3_day_4___power___.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/week_3_day_4___power___.dir/main.cpp.o"
+  "week_3_day_4___power___.pdb"
+  "week_3_day_4___power___"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/week_3_day_4___power___.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
