34c2c5368e882a04beb805ed85eafc7803b89f5b
mate0905 <mate0905@gmail.com>
2019-02-27 14:06:57 +0100

    Deallocated

---
+++ CLionProjects/Week-6/Day-3 /Deallocated/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// with malloc
+// please allocate a 10 long array and fill it with even numbers then print the whole array
+// please deallocate memory without using free
+
+
+int main()
+{
+    int *pointer = NULL;
+
+    pointer = (int *) malloc(10 * sizeof(int));
+
+    for (int i = 0; i < 10; i++) {
+        pointer[i] = i * 2 + 2;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", pointer[i]);
+    }
+
+    realloc(pointer, 0);
+    return 0;
+}
